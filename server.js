a56e0daf3383709369ed4a17929771224b5165cf
Fred Bolder <fgh.bolder@gmail.com>
2023-10-26 14:38:16 +0200

    static

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from "dotenv";
 import multer from 'multer';
 import path from "path";
+import {fileURLToPath} from 'url';
+
+dotenv.config();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -14,15 +18,21 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 const app = express();
-const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, './')));
+
+const PORT = process.env.PORT || 4000;
 
 app.post('/', upload.single('avatar'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
+    res.send("File uploaded");
 })
 
 app.get('/', (req, res) => {
-    const __dirname = process.cwd();
+    //const __dirname = process.cwd();
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
